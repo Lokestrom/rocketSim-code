09940163ed5771c23e2329df088c27b0a143d6eb
Lokemotiv <lokesh@live.no>
2022-02-08 21:49:18 +0100

    x

--- 3dFullRocketSim/databaseCode/writeVectorsToDatabaseFile.cpp
+++ 3dFullRocketSim/databaseCode/writeVectorsToDatabaseFile.cpp
@@ -1,4 +1,6 @@
 #include "main.hpp"
+#include "algorithm.hpp"
+using namespace arraySorting;
 
 int main(){
     std::string fileName = "D:\\code\\codeProsjekt\\flightControler\\3dFullRocketSim\\airDensityfiles\\normalEarth.txt";
@@ -8,8 +10,8 @@ int main(){
         {1.225, 1.1116, 1.0065, 0.9091, 0.8191, 0.7361, 0.6597, 0.5895, 0.5252, 0.4664, 0.4127, 0.3639, 0.2655, 0.1937, 0.1423, 0.088, 0.0395, 0.018, 0.0132, 0.0082, 0.0039, 0.0019, 0.0014, 0.001, 0.00086, 0.000288, 0.000074, 0.000064, 0.000015, 0.000007, 0.000003, 0.0000005, 0.0000002, 0.0000001}
     };
 
-    for(int i = 0; i < vectorData; i++)
-        vectorData[i] = mergeSort(vectorData[i]);
+    for(int i = 0; i < vectorData.size(); i++)
+        mergeSort(vectorData[i]);
 
     std::vector<std::string> *temp = new std::vector<std::string>();
 
---
+++ algorithm.hpp
@@ -0,0 +1,101 @@
+#ifndef algorithm_hpp
+#define algorithm_hpp
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <random>
+#include <limits>
+
+namespace arraySorting
+{
+    std::vector<double> bubbleSort(std::vector<double> &array)
+    {
+        double temp;
+        for (int i = 0; i < array.size(); i++)
+        {
+            for (int j = i + 1; j < array.size(); j++)
+            {
+                if (array[i] > array[j])
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    std::vector<long double> mergeSort(std::vector<long double> &array)
+    {
+        if (array.size() == 1)
+            return array;
+        std::vector<long double>::const_iterator splitPoint = array.begin() + array.size() / 2;
+        std::vector<long double>::const_iterator begin = array.begin();
+        std::vector<long double>::const_iterator end = array.end();
+        std::vector<long double> x(begin, splitPoint), y(splitPoint++, end);
+        x = mergeSort(x);
+        y = mergeSort(y);
+
+        array = {};
+        int xp = 0, yp = 0;
+        while (x.size() != xp && y.size() != yp)
+        {
+            if (x[xp] < y[yp])
+            {
+                array.push_back(x[xp]);
+                xp++;
+            }
+            else
+            {
+                array.push_back(y[yp]);
+                yp++;
+            }
+        }
+
+        for (int i = xp; i < x.size(); i++)
+        {
+            array.push_back(x[i]);
+        }
+        for (int i = yp; i < y.size(); i++)
+        {
+            array.push_back(y[i]);
+        }
+        return array;
+    }
+}
+namespace search
+{
+    int binarySearch(std::vector<double> arr, double target)
+    {
+        int low = 0;
+        int high = arr.size() - 1;
+        int mid;
+
+        while (low <= high)
+        {
+            mid = (low + high) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] > target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
+}
+
+long double generateRand(long double min, long double max)
+{
+    using namespace std;
+
+    static default_random_engine generator(123123);
+    uniform_real_distribution<long double> distribution(min, max);
+
+    return distribution(generator);
+}
+
+#endif
